1e6c523cb6197193d6119b485f7b0ccec412f28b
nic0o05 <nfranquez05@gmail.com>
2023-01-30 12:55:41 -0300

    Terminada la parte base

--- Reto_1/Gana_a_la_maquina.c
+++ Reto_1/Gana_a_la_maquina.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<time.h>
-correcta;
+
 char player1;
 char player2;
 char num_aleatorio;
@@ -13,117 +13,132 @@ int puntos2 = 0;
 
 
 main(){
+//inicio:;
 srand (time(NULL)); //Hace que el numero siempre sea aleatorio
 num_aleatorio = rand() % (122-97+1) + 97;   //Esta entre 97 y 122
 comienza = rand() % 2+1;    //Elige quien comienza
 //char letra_aleatoria = num_aleatorio + '0';
 printf("%d",num_aleatorio);
-printf("\nLISTO \nYa se escogio una letra, ahora intenta adivinar, quien adivine ganara 10 puntos\n");
+printf("\nLISTO \nYa se escogio una letra, ahora intenta adivinar\n");
 printf("Tu eres el jugador uno y la maquina el 2\n");
 printf("Comienza el jugador %d \n", comienza);
 
 if (comienza == 1)
 {
-    while (num_aleatorio != player1 || num_aleatorio != player2)
+    while (player1 != num_aleatorio || player2 != num_aleatorio)
     {
+    secuencia1:;
+    printf("Tu turno\n");
+    player1 = getchar();
+        if (player1 == num_aleatorio)
+        {
+            printf("Felicitaciones, has ganado\n");
+            getchar();
+            goto fin;
+            
+        }
+        else if (player1 > num_aleatorio)
+        {
+            printf("La letra debe ser menor\n");
+            getchar();
+        }
+        else if (player1 < num_aleatorio){
+            printf("La letra debe ser mayor\n");
+            getchar();
+        }
+        
+
+        
+        printf("Turno del jugador 2\n");
+        
+        srand (time(NULL));
+        player2 = rand() % (122-97+1) + 97;
+        
+        if (player2 == num_aleatorio)
+        {
+            printf("A ganado el BOT, ENTER PARA CONTINUAR\n");
+            getchar();
+            goto fin;
+        }
+        else if (player2 > num_aleatorio)
+        {
+            printf("La letra del BOT es mayor, ENTER PARA CONTINUAR\n");
+            getchar();
+            goto secuencia1;
+        }
+        else if (player2 < num_aleatorio){
+            printf("La letra del BOT es menor, ENTER PARA CONTINUAR\n");
+            getchar();
+            goto secuencia1;
+        }
+        
+            
+        
+    
+    
+    }
+}
+
+if (comienza == 2)
+{
+    while (player1 != num_aleatorio || player2 != num_aleatorio)
+    {
+        
+        printf("Turno del jugador 2\n");
+        
+        
+        
+        if (player2 == num_aleatorio)
+        {
+            printf("A ganado el BOT, ENTER PARA CONTINUAR\n");
+            getchar();
+            goto fin;
+        }
+        else if (player2 > num_aleatorio)
+        {
+            printf("La letra del BOT es mayor, ENTER PARA CONTINUAR\n");
+            getchar();
+            goto secuencia2;
+        }
+        else if (player2 < num_aleatorio){
+            printf("La letra del BOT es menor, ENTER PARA CONTINUAR\n");
+            getchar();
+            goto secuencia2;
+        }
+        
+        secuencia2:;
+        srand (time(NULL));
+        player2 = rand() % (122-97+1) + 97;
+        printf("Tu turno\n");
         player1 = getchar();
         if (player1 == num_aleatorio)
         {
             printf("Felicitaciones, has ganado\n");
+            getchar();
+            goto fin;
+            
         }
-        else if (player1 != num_aleatorio)
+        else if (player1 > num_aleatorio)
         {
-            printf("Suerte en la proxima\n");
+            printf("La letra debe ser menor\n");
+            getchar();
+        }
+        else if (player1 < num_aleatorio){
+            printf("La letra debe ser mayor\n");
+            getchar();
         }
         
+
         
-    }
+        
+        
+            
+        
     
+    
+    }
 }
 
 
-
-
-
-
-
-
-
-/*if (comienza == 1) {
-    
-    while (puntos <= 30 || puntos2 <= 30)
-    {
-    printf("Te toca \n");
-    player1 = getchar();
-    
-    if (num_aleatorio == player1){
-    printf("Felicitaciones\n");
-    printf("Has ganado 10 puntos\n");
-    puntos+10;
-    }
-    
-    else if (num_aleatorio != player1)
-    {
-    printf("Incorrecta\n");
-    printf("Suerte en la proxima\n");
-    }
-    
-    
-    
-    printf("--------------------------------------------------\n");
-
-    printf("Turno del jugador 2\n");
-    player2 = rand() % (122-97+1) + 97;
-
-    if (num_aleatorio == player2){
-    printf("La maquina gano 10 puntos\n");
-    puntos2+10;
-    }
-    
-    else if (num_aleatorio != player2)
-    {
-    printf("Incorrecta\n");
-    printf("Suerte en la proxima\n");
-    }
-    
-    }
-    
-    
-
-
-
-
-
-
-    }
-
-
-
-
-
-
-if (comienza == 1)
-{
-     player1 = getchar();
-     if (letra_aleatoria == player1){
-        printf("Felicitaciones")
-     }
-     
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fin:;
 }
